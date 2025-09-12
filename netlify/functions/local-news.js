e185b4bb73c7d31043f37c37f25c611902f5f3b3
GCGIBEAULT <gcgibeault@gmail.com>
2025-09-12 15:45:10 -0600

    Update local-news.js

--- netlify/functions/local-news.js
+++ netlify/functions/local-news.js
@@ -1,9 +1,14 @@
 // netlify/functions/local-news.js
 
-exports.handler = async () => {
+exports.handler = async (event) => {
+  const { lat, lon } = event.queryStringParameters || {};
+
   return {
     statusCode: 200,
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ working: true })
+    body: JSON.stringify({
+      working: true,
+      received: { lat, lon }
+    })
   };
 };
