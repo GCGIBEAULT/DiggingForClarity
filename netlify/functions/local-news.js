eaf2a4c05a98da55033393d330f8acf7f93c342d
GCGIBEAULT <gcgibeault@gmail.com>
2025-09-12 15:46:39 -0600

    Update local-news.js

--- netlify/functions/local-news.js
+++ netlify/functions/local-news.js
@@ -1,14 +1,21 @@
-// netlify/functions/local-news.js
+const fs   = require("fs");
+const path = require("path");
 
-exports.handler = async (event) => {
-  const { lat, lon } = event.queryStringParameters || {};
+exports.handler = async () => {
+  // Load just the first few entries of your ZIP→city map
+  const zipMap = JSON.parse(
+    fs.readFileSync(path.join(__dirname, "cityziplatlong.json"), "utf8")
+  );
+
+  // Return the first 5 zips so you don’t overflow the browser
+  const sample = Object.keys(zipMap).slice(0, 5).reduce((acc, zip) => {
+    acc[zip] = zipMap[zip].city;
+    return acc;
+  }, {});
 
   return {
     statusCode: 200,
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      working: true,
-      received: { lat, lon }
-    })
+    body: JSON.stringify({ working: true, sample })
   };
 };
