5436b8b54b7c30cc51af8cf3e1a675616cc68d27
GCGIBEAULT <gcgibeault@gmail.com>
2025-09-13 21:19:20 -0600

    Update editor.js

--- netlify/functions/editor.js
+++ netlify/functions/editor.js
@@ -1,4 +1,6 @@
 exports.handler = async function (event) {
+  const { city, zip, lat, lon } = JSON.parse(event.body || "{}");
+
   const snippets = [
     { title: "Local park reopens after renovations" },
     { title: "City council approves new housing plan" },
@@ -8,6 +10,7 @@ exports.handler = async function (event) {
     { title: "Local artist featured in gallery show" },
     { title: "Community cleanup scheduled Saturday" }
   ];
+
   return {
     statusCode: 200,
     headers: {
