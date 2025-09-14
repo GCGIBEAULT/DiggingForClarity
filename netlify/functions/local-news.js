6c7973b44f9597725aedfe46f9dfad1816ab247a
GCGIBEAULT <gcgibeault@gmail.com>
2025-09-14 09:52:38 -0600

    Update local-news.js

--- netlify/functions/local-news.js
+++ netlify/functions/local-news.js
@@ -87,14 +87,11 @@ exports.handler = async function(event) {
       snippet: summary || description || text || ""
     }));
 
-    return {
+      return {
       statusCode: 200,
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(cleanHeadlines)
+      body: JSON.stringify(curated),
     };
+
   } catch (err) {
     console.error("Function error:", err.message);
     return {
